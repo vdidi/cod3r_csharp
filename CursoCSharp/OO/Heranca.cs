581700e3e7ddec19e3706682faeb4616753e2565
vdidi <vitor_hugo_di@hotmail.com>
2021-04-30 08:43:23 -0300

    feat: add project

---
+++ CursoCSharp/OO/Heranca.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.OO {
+    class Heranca {
+
+        public class Carro {
+            protected readonly int VelocidadeMaxima;
+            int VelocidadeAtual;
+
+            public Carro(int velocidadeMaxima) {
+                VelocidadeMaxima = velocidadeMaxima;
+            }
+
+            protected int AlterarVelocidade(int delta) {
+                int novaVelocidade = VelocidadeAtual + delta;
+
+                if(novaVelocidade < 0) {
+                    VelocidadeAtual = 0;
+                } else if (novaVelocidade > VelocidadeMaxima) {
+                    VelocidadeAtual = VelocidadeMaxima;
+                } else {
+                    VelocidadeAtual = novaVelocidade;
+                }
+
+                return VelocidadeAtual;
+            }
+
+            public int Acelerar() {
+                return AlterarVelocidade(5);
+            }
+
+            public int Frear() {
+                return AlterarVelocidade(-5);
+            }
+        }
+
+        public class Uno : Carro {
+            public Uno() : base(200) {
+
+            }
+        }
+
+        public static void Executar() {
+            Console.WriteLine("Uno...");
+            Uno c1 = new Uno();
+            Console.WriteLine(c1.Acelerar());
+            Console.WriteLine(c1.Acelerar());
+            Console.WriteLine(c1.Frear());
+            Console.WriteLine(c1.Frear());
+        }
+    }
+}
---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+using CursoCSharp.OO;
+using CursoCSharp.MetodosEFuncoes;
+
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                // OO
+                { "Herança - OO", Heranca.Executar},
+                // Métodos e funções
+                { "Exemplo Lambda - Métodos e funções", ExemploLambda.Executar},
+                { "Lambdas com Delegates - Métodos e funções", ExemploLambda.Executar}
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
